0cc25fb233495a2fc7634446370fe0927b65f722
Guilherme Dantas <guilherme.extreme10@gmail.com>
2022-02-09 08:50:56 -0300

    .

---
+++ exercicios/exe006/exe007.js
@@ -0,0 +1,7 @@
+var vel = 69
+console.log(`A velocidade do seu carro é ${vel} KM/H`)
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade, foi multado.`)   
+}
+
+console.log(`Dirija sempre utilizando o cinto de segurança!`)
