cf4ce8f71366f80842d4de886f3330d119752158
Guilherme Dantas <guilherme.extreme10@gmail.com>
2022-02-14 06:18:59 -0300

    .

---
+++ exercicios/exe008/exe014.js
@@ -0,0 +1,6 @@
+//Laços
+var c = 1
+while (c <= 500){
+    console.log('meu ovo pcr!!!')
+    c++
+}
---
+++ exercicios/exe008/exe015.js
@@ -0,0 +1,6 @@
+//Laço For
+for (var a=1; a < 10; a++) {
+    
+    console.log('Tchubiraun daun')
+
+}
